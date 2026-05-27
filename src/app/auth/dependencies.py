85276d75f7ee2e977c2591cfe1f1d1a420e95d45
Lucas Beneylton <47756391+lbeneylton@users.noreply.github.com>
2026-05-27 00:39:34 -0300

    dev: dominio auth criado

---
+++ src/app/auth/dependencies.py
@@ -0,0 +1,17 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer
+from app.core.jwt import decode_token
+
+security = HTTPBearer()
+
+
+def get_current_user(credentials=Depends(security)):
+    token = credentials.credentials
+
+    payload = decode_token(token)
+
+    if not payload:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
+    return payload
---
+++ src/app/auth/service.py
@@ -0,0 +1,33 @@
+from src.app.users.repository import UserRepository
+
+from src.app.core.hash import verify_password
+from src.app.core.jwt import create_access_token
+
+
+class AuthService:
+    def __init__(self, user_repository: UserRepository):
+        self.user_repository = user_repository
+
+    def login(self, email: str, password: str):
+        user = self.user_repository.get_active_by_email(email)
+
+        # Verifica se o email está ativo
+        if not user:
+            raise Exception("Invalid credentials")
+
+        # Verifica se a senha está correta
+        if not verify_password(password, user.password_hash):
+            raise Exception("Invalid credentials")
+
+        # Cria o token
+        token = create_access_token(
+            data={
+                "sub": str(user.user_id),
+                "email": user.email
+            }
+        )
+
+        return {
+            "access_token": token,
+            "token_type": "bearer",
+        }
